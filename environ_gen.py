52e68f75cffed80436a52d5d7ce8062d9fceacb9
Chetan Gadgil <c.a.gadgil@gmail.com>
2019-02-06 10:49:03 +0100

    Update environ_gen.py

--- environ_gen.py
+++ environ_gen.py
@@ -20,6 +20,23 @@ register(
 envs_init_py_template = Template("""from gym_foo.envs.${env_name}_env import ${cap_env_name}Env
 """)
 
+envs_main_py_template = Template("""import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class FooEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self):
+    pass
+  def step(self, action):
+    pass
+  def reset(self):
+    pass
+  def render(self, mode='human', close=False):
+    pass
+""")
+
 # From https://medium.com/@apoddar573/making-your-own-custom-environment-in-gym-c3b65ff8cdaa
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
